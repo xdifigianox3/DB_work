6eb9d2b96ea2408526b4a6b34b3efd15d2ea77e9
xdifigianox3 <gayer47@googlemail.com>
2020-10-29 08:02:12 +0000

    update7

--- DB_work/03_SQL_UserDB/03_1_userDB.sql
+++ DB_work/03_SQL_UserDB/03_1_userDB.sql
@@ -1,12 +1,33 @@
-/* Tabelle mit USER-Daten */
+/* Tabelle mit user-Daten */
 
-USE work;
+/* Unbedingtes Löschen*/
+/*DROP TABLE users;*/
 
-CREATE TABLE users;
+system clear;
+
+/* Bedingtes Löschen*/
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
 (
-    first_name VARCHAR(20)
-    family_name VARCHAR(20)
-)
+    id INT NOT NULL,
+    user_name VARCHAR(20) NOT NULL PRIMARY KEY,
+    first_name VARCHAR(20) NOT NULL,
+    family_name VARCHAR(20) NOT NULL
+);
+
+#DESC users;
+
+INSERT INTO users (id,user_name,first_name,family_name) VALUES(1,"max","Max","Mütze");
+INSERT INTO users (id,user_name,first_name,family_name) VALUES(2,"max01","Max","Martinelli");
+INSERT INTO users (id,user_name,first_name,family_name) VALUES(3,"max29","Max","Martinelli");
+
+ALTER TABLE users ADD plz INT(5) NOT NULL DEFAULT 12345;
 
 DESC users;
+SELECT * FROM users;
 
+/*
+INSERT INTO users (user_name,first_name,family_name) VALUES("max","Max","Martinelli");
+SELECT * FROM users;
+*/
